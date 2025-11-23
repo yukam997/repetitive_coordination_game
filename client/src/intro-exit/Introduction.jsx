0ced00b333057fc94ec9d00edb6aaf01abdd9a30
Yuka <myuhka12@gmail.com>
2025-11-22 22:23:27 -0800

    changed parking instructions and png

--- client/src/intro-exit/Introduction.jsx
+++ client/src/intro-exit/Introduction.jsx
@@ -11,12 +11,12 @@ export function Introduction({ next }) {
         <p>
           In this experiment, you will play a car parking game with another participant. Your task is to choose where to park your car, but <strong>each parking spot has a different cost</strong>. Additionally, the cost of the parking spot also depends on where your partner parked:
         </p>
-        <p>1. If you and your partner park in the <strong>same spot</strong>, you will incur <strong>a penalty cost of 30.</strong></p>
-        <p>2. If you and your partner park in different spots of the <strong>same color</strong>, you will receive <strong>a group discount of 10.</strong></p>
-        <p>The bonus payment for this experiment depends on the total cost across all rounds in the game. This will be calculated based on this equation:</p>
-        <p> <strong>$5 – (total cost)*$0.01 </strong></p>
-        <p>In other words, your goal is to <strong>minimize the total cost</strong> you pay in this parking game.</p>
-
+        <p>1. If you and your partner park in the <strong>same spot</strong>, you will pay <strong>a penalty of 30MU.</strong></p>
+        <p>2. If you and your partner park in different spots of the <strong>same color</strong>, you will receive <strong>a group discount of 10MU.</strong></p>
+        <p>3. The regular price and the discounted price will be written on the parking spot.</p>
+        <p>The bonus payment for this experiment depends on your total MU across all rounds in the game. This will be calculated based on this equation:</p>
+        <p> <strong>$5 – (total MU you paid)*$0.01 </strong></p>
+        <p>In other words, your goal is to <strong>minimize the total MU</strong> you pay in this parking game.</p>
       </div>
       <Button handleClick={next} autoFocus>
         <p>Next</p>
